cfe7988ef1eec0e31e777ba0bf22adb9b639d71b
toastycoder0 <re-jos2002@proton.me>
2025-09-30 17:55:26 -0600

    feat: create ROUTE_JSON constant for routing data structure and waypoints definition

---
+++ src/lib/constants/route.ts
@@ -0,0 +1,47 @@
+export const ROUTE_JSON = {
+  code: 'Ok',
+  routes: [
+    {
+      legs: [{ steps: [], weight: 88.7, summary: '', duration: 88.7, distance: 722.8 }],
+      weight_name: 'routability',
+      geometry: {
+        coordinates: [
+          [-94.556109, 18.00477],
+          [-94.556031, 18.004726],
+          [-94.555692, 18.004533],
+          [-94.556163, 18.004043],
+          [-94.556248, 18.003955],
+          [-94.556138, 18.003852],
+          [-94.556065, 18.003928],
+          [-94.555585, 18.004429],
+          [-94.555155, 18.004879],
+          [-94.554533, 18.005528],
+          [-94.554002, 18.006083],
+          [-94.554114, 18.006177],
+          [-94.55434, 18.005941],
+          [-94.555021, 18.006317],
+          [-94.555547, 18.006608],
+          [-94.55583, 18.006764]
+        ],
+        type: 'LineString'
+      },
+      weight: 88.7,
+      duration: 88.7,
+      distance: 722.8
+    }
+  ],
+  waypoints: [
+    {
+      hint: 'uz8VhHaC3Yo0AAAADgAAAAAAAAAAAAAAxjoSQnZpGUEAAAAAAAAAADQAAAAOAAAAAAAAAAAAAAC3dAAAMzBd-iK7EgEEMV36bLwSAQAAjwcAAAAA',
+      location: [-94.556109, 18.00477],
+      name: 'Calle 21 de Marzo',
+      distance: 42.7063239
+    },
+    {
+      hint: 'Dg_AhxMPwIc1AAAAMgAAAAAAAAAAAAAAw74TQjxXCkIAAAAAAAAAADUAAAAyAAAAAAAAAAAAAAC3dAAASjFd-uzCEgF-L136-r8SAQAAfwcAAAAA',
+      location: [-94.55583, 18.006764],
+      name: 'Avenida Tecnológico',
+      distance: 96.62840756
+    }
+  ]
+}
