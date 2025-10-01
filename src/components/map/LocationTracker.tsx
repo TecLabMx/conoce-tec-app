ad79b74a52394a46332646e7627cfdac3273811f
toastycoder0 <re-jos2002@proton.me>
2025-10-01 15:46:56 -0600

    fix: enhance LocationTracker UI with real-time location feedback and error handling features for improved user experience

--- src/components/map/LocationTracker.tsx
+++ src/components/map/LocationTracker.tsx
@@ -1,18 +1,24 @@
 import { useGeolocation } from '@uidotdev/usehooks'
-import { Navigation, CircleAlert } from 'lucide-react'
-import { Button } from '../ui/Button'
+import { Navigation, CircleAlert, Pin, ChevronRight } from 'lucide-react'
 import { Marker } from '@vis.gl/react-maplibre'
+import { Input } from '../ui/Input'
 
 const LocationTracker = () => {
   const { loading, error, latitude, longitude } = useGeolocation()
 
   return (
     <>
-      {loading && (
-        <section className='fixed top-0 left-0 w-full flex h-full z-[1] bg-black/50'>
-          <div className='m-auto w-fit p-12 bg-white rounded-lg'>
+      {latitude && longitude && (
+        <Marker longitude={longitude} latitude={latitude}>
+          <Navigation className='fill-red-400 stroke-red-600' />
+        </Marker>
+      )}
+
+      <div className='w-full max-w-md h-full fixed top-0 left-0 z-10 p-4'>
+        <aside className='w-full h-full flex flex-col gap-4 bg-white overflow-y-auto rounded-lg border-slate-200 border px-4 py-6'>
+          {loading ? (
             <div
-              className='size-28 bg-gradient-to-b from-transparent via-white to-transparent bg-[length:25px_400%] bg-no-repeat animate-matrix'
+              className='size-28 bg-gradient-to-b from-transparent via-white to-transparent bg-[length:25px_400%] bg-no-repeat animate-matrix m-auto'
               style={{
                 backgroundImage: `
                 linear-gradient(#0000 calc(1*100%/6),#000 0 calc(3*100%/6),#0000 0),
@@ -20,26 +26,36 @@ const LocationTracker = () => {
                 linear-gradient(#0000 calc(3*100%/6),#000 0 calc(5*100%/6),#0000 0)`
               }}
             />
-          </div>
-        </section>
-      )}
-      {error ? (
-        <div className='w-full top-0 left-0 p-4 fixed'>
-          <section className='w-fit max-w-full bg-red-50 border-2 text-red-600 border-red-200 p-4 rounded-lg flex gap-2 items-center text-sm'>
-            <CircleAlert size={22} />
-            <p className='flex break-after-all'>Sucedio un error al obtener la ubicación actual</p>
-          </section>
-        </div>
-      ) : (
-        <>
-          <Marker longitude={longitude} latitude={latitude}>
-            <Navigation className='fill-red-400 stroke-red-600' />
-          </Marker>
-          <Button variant='outline' className='fixed left-4 bottom-4'>
-            <Navigation className='fill-red-400 stroke-red-600' />
-          </Button>
-        </>
-      )}
+          ) : (
+            <>
+              <Input placeholder='Filtrar por nombre del lugar' className='w-full' />
+              {error ? (
+                <span className='text-red-600 flex gap-2 items-center bg-red-50 p-2 border border-red-400 rounded-lg'>
+                  <CircleAlert size={20} />
+                  <p className='text-sm'>Tu ubicación no puede ser determinada. Por favor intentalo de nuevo.</p>
+                </span>
+              ) : (
+                <span className='text-green-600 flex gap-2 items-center bg-red-50 p-2 border border-green-400 rounded-lg'>
+                  <CircleAlert size={20} />
+                  <p className='text-sm'>Tu ubicación esta siendo obtenida en tiempo real.</p>
+                </span>
+              )}
+              {Array.from({ length: 6 }).map((_, i) => (
+                <section className='w-full rounded-lg flex gap-4 border-slate-200 border p-4 items-center' key={i}>
+                  <div className='aspect-square w-11 flex bg-[#F2F3F2] text-black/50 rounded-lg'>
+                    <Pin className='m-auto' size={18} />
+                  </div>
+                  <div>
+                    <p className='text-base font-medium'>Laboratorio</p>
+                    <p className='text-black/50 text-sm'>0.4 mil</p>
+                  </div>
+                  <ChevronRight className='text-black/50 ml-auto' />
+                </section>
+              ))}
+            </>
+          )}
+        </aside>
+      </div>
     </>
   )
 }
