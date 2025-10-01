380b0667945caa695f70efa2490f4012e3070dca
toastycoder0 <re-jos2002@proton.me>
2025-09-30 18:01:40 -0600

    feat: extract INITIAL_VIEW_STATE constant for cleaner map initialization in app component

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,12 @@
-import { Navigation } from 'lucide-react'
 import { Map } from '@vis.gl/react-maplibre'
+import { Navigation } from 'lucide-react'
 import { Button } from './components/ui/Button'
+import { INITIAL_VIEW_STATE } from './lib/constants/map'
 
 const App = () => {
   return (
     <>
-      <Map
-        initialViewState={{
-          longitude: -94.556299,
-          latitude: 18.005684,
-          zoom: 17
-        }}
-        mapStyle='https://tiles.openfreemap.org/styles/liberty'
-      />
+      <Map initialViewState={INITIAL_VIEW_STATE} mapStyle='https://tiles.openfreemap.org/styles/liberty' />
 
       <Button className='fixed left-4 bottom-4 z-10'>
         <Navigation />
---
+++ src/lib/constants/map.ts
@@ -0,0 +1,5 @@
+export const INITIAL_VIEW_STATE = {
+  longitude: -94.557891,
+  latitude: 18.00682,
+  zoom: 17
+}
