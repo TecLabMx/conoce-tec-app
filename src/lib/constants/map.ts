d3a71b9087aada33405b26bdb7ba48e0bd97cd82
toastycoder0 <re-jos2002@proton.me>
2025-09-30 23:43:31 -0600

    feat: add initial and final marker classes for improved visualization in the map application

--- src/lib/constants/map.ts
+++ src/lib/constants/map.ts
@@ -3,3 +3,6 @@ export const INITIAL_VIEW_STATE = {
   latitude: 18.00682,
   zoom: 17
 }
+
+export const INITIAL_MARKER_CLASSES = 'fill-green-400 stroke-green-600'
+export const FINAL_MARKER_CLASSES = 'fill-blue-400 stroke-blue-600'
