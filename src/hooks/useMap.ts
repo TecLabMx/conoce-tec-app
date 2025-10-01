03d1bc0994ebc90430c0aa6d265b946f6a85c447
toastycoder0 <re-jos2002@proton.me>
2025-09-30 23:55:30 -0600

    feat: implement useMap hook for managing markers and routes in the map, enabling dynamic route fetching and state management

---
+++ src/hooks/useMap.ts
@@ -0,0 +1,30 @@
+import type { RouteFeature } from '../validations/geojson'
+import type { MarkerItem } from '../types/misc'
+import { useState } from 'react'
+import { fetchRoute } from '../lib/osrm'
+
+const useMap = () => {
+  const [markers, setMarkers] = useState<MarkerItem[]>([])
+  const [route, setRoute] = useState<RouteFeature | null>(null)
+
+  const onAddMarker = async (markerLocation: MarkerItem) => {
+    const { lng, lat } = markerLocation
+
+    if (markers.length === 2) {
+      setMarkers([])
+      setRoute(null)
+      return
+    }
+
+    setMarkers([...markers, { lng, lat }])
+
+    if (markers.length === 1) {
+      const route = await fetchRoute({ start: markers[0], end: { lng, lat } })
+      setRoute(route)
+    }
+  }
+
+  return { markers, route, onAddMarker }
+}
+
+export { useMap }
