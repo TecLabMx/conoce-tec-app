d78042a2f8cac3f44c6ccec3f1cddd4348225754
toastycoder0 <re-jos2002@proton.me>
2025-10-01 00:30:32 -0600

    refactor: restructure INITIAL_VIEW_STATE to INITIAL_MAP_STATES, adding maxBounds for improved map configuration and management

--- src/lib/constants/map.ts
+++ src/lib/constants/map.ts
@@ -8,10 +8,16 @@ export const DATA_LAYER: LayerProps = {
   }
 }
 
-export const INITIAL_VIEW_STATE = {
-  longitude: -94.557891,
-  latitude: 18.00682,
-  zoom: 17
+export const INITIAL_MAP_STATES = {
+  initialViewState: {
+    longitude: -94.557891,
+    latitude: 18.00682,
+    zoom: 17
+  },
+  maxBounds: [
+    [-94.565, 18.0],
+    [-94.55, 18.015]
+  ]
 }
 
 export const INITIAL_MARKER_CLASSES = 'fill-green-400 stroke-green-600'
