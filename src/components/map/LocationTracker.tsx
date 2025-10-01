733223117f8281c5543ea030556adea8006b0c33
toastycoder0 <re-jos2002@proton.me>
2025-10-01 14:44:50 -0600

    chore: refactor LocationTracker to use useGeolocation hook and enhance loading/error handling for location retrieval

--- src/components/map/LocationTracker.tsx
+++ src/components/map/LocationTracker.tsx
@@ -1,41 +1,46 @@
-import { useEffect } from 'react'
-import { Navigation } from 'lucide-react'
+import { useGeolocation } from '@uidotdev/usehooks'
+import { Navigation, CircleAlert } from 'lucide-react'
 import { Button } from '../ui/Button'
-import { toast } from 'sonner'
+import { Marker } from '@vis.gl/react-maplibre'
 
 const LocationTracker = () => {
-  useEffect(() => {
-    const onGetLocation = async () => {
-      try {
-        const permission = await navigator.permissions.query({ name: 'geolocation' })
-
-        if (permission.state === 'granted') {
-          navigator.geolocation.getCurrentPosition(
-            (pos) => {
-              const lat = pos.coords.latitude
-              const lon = pos.coords.longitude
-              toast.success(`Ubicación obtenida: lat ${lat}, lon ${lon}`)
-            },
-            (error) => {
-              toast.error('Error al obtener la ubicación. ' + error.message)
-            }
-          )
-        } else {
-          toast.error('Necesitas habilitar la localización para poder ver tu ubicación actual.')
-        }
-      } catch (error) {
-        console.error(error)
-        toast.error('Error al consultar permisos de geolocalización.')
-      }
-    }
-
-    onGetLocation()
-  }, [])
+  const { loading, error, latitude, longitude } = useGeolocation()
 
   return (
-    <Button variant='outline' className='fixed left-4 bottom-4 z-10'>
-      <Navigation className='fill-red-400 stroke-red-600' />
-    </Button>
+    <>
+      {loading && (
+        <section className='fixed top-0 left-0 w-full flex h-full z-[1] bg-black/50'>
+          <div className='m-auto w-fit p-12 bg-white rounded-lg'>
+            <div
+              className='size-28 bg-gradient-to-b from-transparent via-white to-transparent bg-[length:25px_400%] bg-no-repeat animate-matrix'
+              style={{
+                backgroundImage: `
+                linear-gradient(#0000 calc(1*100%/6),#000 0 calc(3*100%/6),#0000 0),
+                linear-gradient(#0000 calc(2*100%/6),#000 0 calc(4*100%/6),#0000 0),
+                linear-gradient(#0000 calc(3*100%/6),#000 0 calc(5*100%/6),#0000 0)`
+              }}
+            />
+          </div>
+        </section>
+      )}
+      {error ? (
+        <div className='w-full top-0 left-0 p-4 fixed'>
+          <section className='w-fit max-w-full bg-red-50 border-2 text-red-600 border-red-200 p-4 rounded-lg flex gap-2 items-center text-sm'>
+            <CircleAlert size={22} />
+            <p className='flex break-after-all'>Sucedio un error al obtener la ubicación actual</p>
+          </section>
+        </div>
+      ) : (
+        <>
+          <Marker longitude={longitude} latitude={latitude}>
+            <Navigation className='fill-red-400 stroke-red-600' />
+          </Marker>
+          <Button variant='outline' className='fixed left-4 bottom-4'>
+            <Navigation className='fill-red-400 stroke-red-600' />
+          </Button>
+        </>
+      )}
+    </>
   )
 }
 
