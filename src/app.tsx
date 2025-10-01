dbe083a34dc2b0b753b154dc7eae1c08567e3ebc
toastycoder0 <re-jos2002@proton.me>
2025-10-01 01:19:25 -0600

    refactor: simplify App component by removing unnecessary fragments and streamlining Map props for clarity and maintainability

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,24 @@
 import { Map, Marker, Source, Layer } from '@vis.gl/react-maplibre'
-import { Navigation, Pin } from 'lucide-react'
-import { Button } from './components/ui/Button'
-import { INITIAL_VIEW_STATE, INITIAL_MARKER_CLASSES, FINAL_MARKER_CLASSES, DATA_LAYER } from './lib/constants/map'
+import { Pin } from 'lucide-react'
+import { INITIAL_MAP_STATES, INITIAL_MARKER_CLASSES, FINAL_MARKER_CLASSES, DATA_LAYER } from './lib/constants/map'
 import { useMapRoutes } from './hooks/useMapRoutes'
 
 const App = () => {
   const { markers, route, onAddMarker } = useMapRoutes()
 
   return (
-    <>
-      <Map
-        onClick={async ({ lngLat }) => await onAddMarker(lngLat)}
-        initialViewState={INITIAL_VIEW_STATE}
-        mapStyle='https://tiles.openfreemap.org/styles/liberty'
-      >
-        {route && (
-          <Source id='route' type='geojson' data={route}>
-            <Layer {...DATA_LAYER} />
-          </Source>
-        )}
-        {markers.map(({ lng, lat }, i) => (
-          <Marker key={lng + lat} longitude={lng} latitude={lat}>
-            <Pin className={i === 0 ? INITIAL_MARKER_CLASSES : FINAL_MARKER_CLASSES} />
-          </Marker>
-        ))}
-      </Map>
-
-      <Button variant='outline' className='fixed left-4 bottom-4 z-10'>
-        <Navigation className='fill-red-400 stroke-red-600' />
-      </Button>
-    </>
+    <Map onClick={async ({ lngLat }) => await onAddMarker(lngLat)} mapStyle='https://tiles.openfreemap.org/styles/liberty' {...INITIAL_MAP_STATES}>
+      {route && (
+        <Source id='route' type='geojson' data={route}>
+          <Layer {...DATA_LAYER} />
+        </Source>
+      )}
+      {markers.map(({ lng, lat }, i) => (
+        <Marker key={lng + lat} longitude={lng} latitude={lat}>
+          <Pin className={i === 0 ? INITIAL_MARKER_CLASSES : FINAL_MARKER_CLASSES} />
+        </Marker>
+      ))}
+    </Map>
   )
 }
 
