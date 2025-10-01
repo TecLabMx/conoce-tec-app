f025880b419d217210d74b92395dbe28d2d88bd4
toastycoder0 <re-jos2002@proton.me>
2025-09-30 19:04:23 -0600

    feat: implement fetchRoute function to retrieve and process walking routes using the OSRM API for improved navigation

---
+++ src/lib/osrm.ts
@@ -0,0 +1,53 @@
+import { toast } from 'sonner'
+import { OSRMResponseSchema, RouteFeature } from '../validations/geojson'
+
+export interface FetchRouteProps {
+  start: {
+    lat: number
+    lng: number
+  }
+  end: {
+    lat: number
+    lng: number
+  }
+}
+
+const ERROR_MESSAGE = 'Ocurrió un error al trazar la ruta'
+
+export const fetchRoute = async ({ start, end }: FetchRouteProps) => {
+  const URL = `https://router.project-osrm.org/route/v1/walking/${start.lat},${start.lng};${end.lat},${end.lng}?overview=full&geometries=geojson`
+  const res = await fetch(URL).then((res) => res.json())
+
+  const { data, error } = OSRMResponseSchema.safeParse(res)
+
+  if (error) {
+    toast.error(ERROR_MESSAGE)
+    return null
+  }
+
+  const { routes, waypoints } = data
+
+  const route = routes[0]
+
+  if (!route) {
+    toast.error(ERROR_MESSAGE)
+    return null
+  }
+
+  const startWaypoint = waypoints[0]
+  const endWaypoint = waypoints[1]
+
+  const processedRoute: RouteFeature = {
+    type: 'Feature',
+    geometry: route.geometry,
+    properties: {
+      distance: route.distance,
+      duration: route.duration,
+      start_name: startWaypoint.name,
+      end_name: endWaypoint.name,
+      weight: route.weight
+    }
+  }
+
+  return processedRoute
+}
