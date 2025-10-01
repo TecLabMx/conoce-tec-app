719bdfe2849a4c77744d1d739c57c0598a83de34
toastycoder0 <re-jos2002@proton.me>
2025-09-30 23:57:01 -0600

    feat: improve error handling and response validation in fetchRoute for route fetching to enhance reliability and user feedback

--- src/lib/osrm.ts
+++ src/lib/osrm.ts
@@ -15,39 +15,45 @@ export interface FetchRouteProps {
 const ERROR_MESSAGE = 'Ocurrió un error al trazar la ruta'
 
 export const fetchRoute = async ({ start, end }: FetchRouteProps) => {
-  const URL = `https://router.project-osrm.org/route/v1/walking/${start.lng},${start.lat};${end.lng},${end.lat}?overview=full&geometries=geojson`
-  const res = await fetch(URL).then((res) => res.json())
+  try {
+    const URL = `https://router.project-osrm.org/route/v1/walking/${start.lng},${start.lat};${end.lng},${end.lat}?overview=full&geometries=geojson`
+    const res = await fetch(URL).then((res) => res.json())
 
-  const { data, error } = OSRMResponseSchema.safeParse(res)
+    const { data, error } = OSRMResponseSchema.safeParse(res)
 
-  if (error) {
-    toast.error(ERROR_MESSAGE)
-    return null
-  }
-
-  const { routes, waypoints } = data
-
-  const route = routes[0]
-
-  if (!route) {
-    toast.error(ERROR_MESSAGE)
-    return null
-  }
-
-  const startWaypoint = waypoints[0]
-  const endWaypoint = waypoints[1]
-
-  const processedRoute: RouteFeature = {
-    type: 'Feature',
-    geometry: route.geometry,
-    properties: {
-      distance: route.distance,
-      duration: route.duration,
-      start_name: startWaypoint.name,
-      end_name: endWaypoint.name,
-      weight: route.weight
+    if (error) {
+      toast.error(ERROR_MESSAGE)
+      return null
     }
-  }
 
-  return processedRoute
+    const { routes, waypoints } = data
+
+    const route = routes[0]
+
+    if (!route) {
+      toast.error(ERROR_MESSAGE)
+      return null
+    }
+
+    const startWaypoint = waypoints[0]
+    const endWaypoint = waypoints[1]
+
+    const processedRoute: RouteFeature = {
+      type: 'Feature',
+      geometry: route.geometry,
+      properties: {
+        distance: route.distance,
+        duration: route.duration,
+        start_name: startWaypoint.name,
+        end_name: endWaypoint.name,
+        weight: route.weight
+      }
+    }
+
+    return processedRoute
+  } catch (error) {
+    console.error(error)
+    toast.error(ERROR_MESSAGE)
+    return null
+  }
 }
