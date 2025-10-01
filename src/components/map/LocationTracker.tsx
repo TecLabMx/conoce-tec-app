3c0b9ab52c3dc15b3e17b4a9fa11dd6dbbbac735
toastycoder0 <re-jos2002@proton.me>
2025-10-01 10:34:44 -0600

    feat: implement geolocation functionality in LocationTracker with success/error toast notifications for user feedback

--- src/components/map/LocationTracker.tsx
+++ src/components/map/LocationTracker.tsx
@@ -1,7 +1,37 @@
+import { useEffect } from 'react'
 import { Navigation } from 'lucide-react'
 import { Button } from '../ui/Button'
+import { toast } from 'sonner'
 
 const LocationTracker = () => {
+  useEffect(() => {
+    const onGetLocation = async () => {
+      try {
+        const permission = await navigator.permissions.query({ name: 'geolocation' })
+
+        if (permission.state === 'granted') {
+          navigator.geolocation.getCurrentPosition(
+            (pos) => {
+              const lat = pos.coords.latitude
+              const lon = pos.coords.longitude
+              toast.success(`Ubicación obtenida: lat ${lat}, lon ${lon}`)
+            },
+            (error) => {
+              toast.error('Error al obtener la ubicación. ' + error.message)
+            }
+          )
+        } else {
+          toast.error('Necesitas habilitar la localización para poder ver tu ubicación actual.')
+        }
+      } catch (error) {
+        console.error(error)
+        toast.error('Error al consultar permisos de geolocalización.')
+      }
+    }
+
+    onGetLocation()
+  }, [])
+
   return (
     <Button variant='outline' className='fixed left-4 bottom-4 z-10'>
       <Navigation className='fill-red-400 stroke-red-600' />
