84e223bb659bc89646a5a31eb7d2668dbf7663ac
toastycoder0 <re-jos2002@proton.me>
2025-09-30 23:55:33 -0600

    feat: enhance Map component to support dynamic markers and route display, utilizing useMap for state management and interaction

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,36 @@
-import { Map } from '@vis.gl/react-maplibre'
-import { Navigation } from 'lucide-react'
+import { Map, Marker, Source, Layer } from '@vis.gl/react-maplibre'
+import { Navigation, Pin } from 'lucide-react'
 import { Button } from './components/ui/Button'
-import { INITIAL_VIEW_STATE } from './lib/constants/map'
+import { INITIAL_VIEW_STATE, INITIAL_MARKER_CLASSES, FINAL_MARKER_CLASSES, DATA_LAYER } from './lib/constants/map'
+import { useMap } from './hooks/useMap'
 
 const App = () => {
+  const { markers, route, onAddMarker } = useMap()
+
   return (
     <>
-      <Map initialViewState={INITIAL_VIEW_STATE} mapStyle='https://tiles.openfreemap.org/styles/liberty' />
+      <Map
+        onClick={async ({ lngLat }) => await onAddMarker(lngLat)}
+        initialViewState={INITIAL_VIEW_STATE}
+        mapStyle='https://tiles.openfreemap.org/styles/liberty'
+      >
+        {route && (
+          <Source id='route' type='geojson' data={route}>
+            <Layer {...DATA_LAYER} />
+          </Source>
+        )}
+        {markers.map(({ lng, lat }, i) => (
+          <Marker key={lng + lat} longitude={lng} latitude={lat}>
+            <Pin className={i === 0 ? INITIAL_MARKER_CLASSES : FINAL_MARKER_CLASSES} />
+          </Marker>
+        ))}
+      </Map>
 
-      <Button className='fixed left-4 bottom-4 z-10'>
-        <Navigation />
+      <Button variant='outline' className='fixed left-4 bottom-4 z-10'>
+        <Navigation className='fill-red-400 stroke-red-600' />
       </Button>
     </>
   )
 }
 
-export default App
+export { App }
